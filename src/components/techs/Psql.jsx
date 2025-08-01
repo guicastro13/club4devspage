46c952ffe1ee322fe82c5336ce1a4f9181ecfac7
Guilherme Castro <guii_1@hotmail.com>
2025-07-31 21:08:24 -0300

    arruma assets

--- src/components/techs/Psql.jsx
+++ src/components/techs/Psql.jsx
@@ -2,7 +2,7 @@ function PsqlLogo() {
     return (
     <div className="flex flex-col items-center gap-2 text-gray-400 hover:text-white transition-colors duration-300">
       <img
-          src="./public/images/psql.png"
+          src="./images/psql.png"
           alt="Ícone Golang"
           className="w-32 h-32"
         />
